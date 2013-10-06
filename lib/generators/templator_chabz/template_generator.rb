a0eae7324987c766dbb29340f9736583fa40e8d4
Charles Darwin D.C. Pobre <cdpobre@yahoo.com>
2013-10-06 22:45:50 +0800

    added foundation templates

--- lib/generators/templator_chabz/template_generator.rb
+++ lib/generators/templator_chabz/template_generator.rb
@@ -3,7 +3,29 @@ require 'rails/generators'
 module TemplatorChabz
   module Generators
     class TemplateGenerator < Rails::Generators::Base
-      
+      ZB_TEMPLATES = ['banded','blog', 'feed', 'grid', 'orbit', 'banner-home', 'sidebar']
+
+      class_option :haml, type: :boolean, desc: "Generate HAML layout"
+      class_option :framework, type: :string, default: "foundation"
+      class_option :template, type: :string, default: "blog"
+
+      def self.source_root
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+      end
+
+      def create_template
+        if ZB_TEMPLATES.include?(options.template) && options.framework == "foundation"
+          if options.haml?
+            remove_file "app/views/layouts/application.html.erb"
+            template "foundation/haml/#{options.template}.html.haml", "app/views/layouts/application.html.haml"
+          else
+            remove_file "app/views/layouts/application.html.haml"
+            template "foundation/erb/#{options.template}.html.erb", "app/views/layouts/application.html.erb"
+          end
+        else
+          raise "template not found."
+        end
+      end
     end
   end
 end
